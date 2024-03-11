d4d906d06f5aff5ed57a2cd3ce564bafc4c3e724
sachan12 <sachinnanda427@gmail.com>
2024-03-11 12:33:54 -0700

    add files

---
+++ main.ts
@@ -0,0 +1,74 @@
+// console.log("Hello World");
+// console.log("Saith Sachan Nanda");
+
+// String data type:
+// console.log("abc");
+// console.log('abc');
+// console.log(`abc`);
+
+// Number data types:
+// console.log(100);
+// console.log(1000000);
+// console.log(5);
+// console.log(0);
+
+// to check type of any data method : we have use ( typeof ):
+// console.log(typeof 'abc');
+// console.log (typeof 100);
+
+// Boolean data type:
+// console.log(true);
+// console.log(false);
+// console.log(2==2);
+
+// console.log('hello','World','this is typescript session');
+// console.log('hello '+'World '+'this is typescript session');  // Concatenation
+
+// variables
+// variable decleration : let, const , var
+
+// let box1 = 'apple';
+// const container = 'ball';
+// var  myvariable = 100;
+
+// console.log(box1);
+// console.log(typeof box1);
+// console.log(typeof myvariable);
+
+// variable type annotations :
+// let box1:string = 'apple';
+// // box1 = 'orange';
+// const container:string = 'ball';
+// var  myvariable:number = 100;
+// let isthisright:boolean=true;
+// console.log(box1)
+
+// Variable naming convention:
+// let myBoxOneInTheRoom ='flowers' // camel Case (MOSTLY USED)
+// let my_box_one_in_the_room ='flowers' // snake case
+// let MyBoxOneInTheRoom ='flowers' // pascal Case
+ 
+// Updation ;
+// var myBox = 'apples';   // decleration
+// myBox = 'Orange';
+// myBox = 'Peech';
+// console.log(myBox);
+
+// var myBox = 'ball'; // again decleration
+
+// let yourBox = 'flowers'; // decleration
+// yourBox = 'apples';   // Update
+// let yourBox = 'peech'; //redeclare (No)
+
+// const laptop = 'files'; // Decleration
+// // laptop = 'movies'; // update (NO)
+// // const laptop = 'file';// redeclare (NO)
+// let x = 10;
+// if (x===10){
+//     let myBox = 'flowers';
+//     const hisBox = 'peeches';
+//     var yourBox = 'apples';
+//     console.log(myBox)
+//     console.log(hisBox);
+// }
+// // console.log(yourBox);
